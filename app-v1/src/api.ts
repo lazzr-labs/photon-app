3fa5311985d4ff762b06d5ef03924f3615f5bf9c
Miguel Vazquez <migueldv90@gmail.com>
2026-07-08 11:03:27 -0500

    update

--- app-v1/src/api.ts
+++ app-v1/src/api.ts
@@ -1,12 +1,13 @@
 import axios from 'axios';
 
-import { AuthApi, UsersApi, Configuration } from './openapi';
+import { AuthApi, Configuration, PlacesApi, UsersApi } from './openapi';
 
 const config = new Configuration();
 const axiosInstance = axios.create();
 const url = process.env.EXPO_PUBLIC_API_URL;
 
 const authApi = new AuthApi(config, url, axiosInstance);
+const placesApi = new PlacesApi(config, url, axiosInstance);
 const usersApi = new UsersApi(config, url, axiosInstance);
 
-export { authApi, usersApi };
+export { authApi, placesApi, usersApi };
---
+++ app-v1/src/scripts/places.ts
@@ -0,0 +1,20 @@
+import { placesApi } from '~/src/api';
+import type { AddressPlace, AddressPlaceAutocomplete } from '~/src/openapi';
+
+export const placesGet = async (search: string, authToken: string): Promise<AddressPlaceAutocomplete[] | null> => {
+  try {
+    const response = await placesApi.placesGetAPI(authToken, search);
+    return response.data.list;
+  } catch {
+    return null;
+  }
+};
+
+export const placeGet = async (placeID: string, authToken: string): Promise<AddressPlace | null> => {
+  try {
+    const response = await placesApi.placeGetAPI(placeID, authToken);
+    return response.data.object;
+  } catch {
+    return null;
+  }
+};
