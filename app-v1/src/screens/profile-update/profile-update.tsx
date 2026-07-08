57dfa6869cba4e0fdf1968c36843cf01ea994b20
Miguel Vazquez <migueldv90@gmail.com>
2026-07-08 10:36:38 -0500

    update

--- app-v1/src/screens/profile-update/profile-update.tsx
+++ app-v1/src/screens/profile-update/profile-update.tsx
@@ -6,11 +6,11 @@ import { ProfileUpdateContent } from './profile-update-content';
 
 export const ProfileUpdateScreen = () => {
   return (
-    <SafeAreaView>
+    <SafeAreaView className='flex-1' edges={['top', 'left', 'right']}>
 
       <ProfileUpdateHeader />
 
-      <ScrollView keyboardShouldPersistTaps='handled' className='h-full'>
+      <ScrollView keyboardShouldPersistTaps='handled' className='flex-1' contentContainerClassName='flex-grow'>
         <View className='items-center'>
           <View className='w-full sm:w-1/3'>
             <ProfileUpdateContent />
--- app-v1/src/screens/sign-up/sign-up.tsx
+++ app-v1/src/screens/sign-up/sign-up.tsx
@@ -6,11 +6,11 @@ import { SignUpContent } from './sign-up-content';
 
 export const SignUpScreen = () => {
   return (
-    <SafeAreaView>
+    <SafeAreaView className='flex-1' edges={['top', 'left', 'right']}>
 
       <SignUpHeader />
 
-      <ScrollView keyboardShouldPersistTaps='handled' className='h-full'>
+      <ScrollView keyboardShouldPersistTaps='handled' className='flex-1' contentContainerClassName='flex-grow'>
         <View className='items-center'>
           <View className='w-full pt-16 sm:w-1/3'>
             <SignUpContent />
