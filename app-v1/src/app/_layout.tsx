a50350361403e67c141b206b5021b0a3618633c5
Miguel Vazquez <migueldv90@gmail.com>
2026-07-08 10:30:54 -0500

    update

--- app-v1/src/app/_layout.tsx
+++ app-v1/src/app/_layout.tsx
@@ -5,6 +5,7 @@ import { useState, useEffect } from 'react';
 import { SplashScreen, Stack } from 'expo-router';
 import { PortalHost } from '@rn-primitives/portal';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { KeyboardProvider } from 'react-native-keyboard-controller';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { Theme, ThemeProvider, DefaultTheme, DarkTheme } from '@react-navigation/native';
 import { Poppins_400Regular, Poppins_500Medium, Poppins_600SemiBold, Poppins_700Bold } from '@expo-google-fonts/poppins';
@@ -64,15 +65,17 @@ const RootLayout = () => {
 
   return (
     <GestureHandlerRootView>
-      <ThemeProvider value={(colorScheme === 'dark') ? DARK_THEME : LIGHT_THEME}>
-        <StatusBar style={(colorScheme === 'dark') ? 'light' : 'dark'}></StatusBar>
-        <Stack>
-          <Stack.Screen name='(app)' options={{ headerShown: false }}></Stack.Screen>
-          <Stack.Screen name='(auth)' options={{ headerShown: false }}></Stack.Screen>
-        </Stack>
-        <ToastHost />
-        <PortalHost />
-      </ThemeProvider>
+      <KeyboardProvider>
+        <ThemeProvider value={(colorScheme === 'dark') ? DARK_THEME : LIGHT_THEME}>
+          <StatusBar style={(colorScheme === 'dark') ? 'light' : 'dark'}></StatusBar>
+          <Stack>
+            <Stack.Screen name='(app)' options={{ headerShown: false }}></Stack.Screen>
+            <Stack.Screen name='(auth)' options={{ headerShown: false }}></Stack.Screen>
+          </Stack>
+          <ToastHost />
+          <PortalHost />
+        </ThemeProvider>
+      </KeyboardProvider>
     </GestureHandlerRootView>
   );
 };
